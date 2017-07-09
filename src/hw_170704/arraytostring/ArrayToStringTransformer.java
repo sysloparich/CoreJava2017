9620af0c1df344203615c010099e2f809121c2b7
sysloparich <sysloparich@gmail.com>
2017-07-09 15:37:56 +0300

    fixed array to string transformer

--- src/hw_170704/arraytostring/ArrayToStringTransformer.java
+++ src/hw_170704/arraytostring/ArrayToStringTransformer.java
@@ -3,14 +3,18 @@ package hw_170704.arraytostring;
 public class ArrayToStringTransformer {
 	
 	public static void main(String[] args) {
-		int [] test_array = {3,4, 7};
+		int [] test_array = {1,2};
 		toString(test_array);
+		
 	}
 
 	private static void toString(int[] input) {
-		String output = "[" + input[0];
-		for(int i = 1; i < input.length; ++i){
-			output += ", " + input[i];
+		String output = "[";
+		if(input.length != 0){
+			output += input[0]; 
+			for(int i = 1; i < input.length; ++i){
+				output += ", " + input[i];
+			}
 		}
 		output += "]";
 		System.out.println(output);
