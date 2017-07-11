3e36b11810d310d07796917c29c6f95bf3aa4c26
sysloparich <sysloparich@gmail.com>
2017-07-11 16:41:18 +0300

    added stringlinkedlist and toString has been modified

---
+++ src/hw_170710/stringlinkedlist/Node.java
@@ -0,0 +1,13 @@
+package hw_170710.stringlinkedlist;
+
+public class Node {
+	
+	String value;
+	Node next;
+
+	public Node(String value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+
+}
