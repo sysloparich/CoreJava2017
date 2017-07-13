82e99c24247b177a10bff6e9d455e05a55784fb5
sysloparich <sysloparich@gmail.com>
2017-07-13 22:07:06 +0300

    added templates of functions

---
+++ src/hw_170712/StringStack.java
@@ -0,0 +1,42 @@
+package hw_170712;
+
+public class StringStack {
+
+	private String[] elements; 
+	private static final int DEFAULT_MAX_SIZE = 4;
+	private int size;
+
+	StringStack(){
+		this(DEFAULT_MAX_SIZE);
+	}
+	
+	
+	StringStack(int size){
+		this.size = size;
+	}
+	
+	
+	public int size(){
+		return 0;
+	}
+	
+	
+	public String pop(){
+		return null;
+	}
+	
+	
+	public boolean push(String smth){
+		return false;
+	}
+	
+	
+	public String tos(){
+		return null;
+	}
+	
+	
+	public String toString(){
+		return super.toString();
+	}
+}
---
+++ src/hw_170712/UseStringStack.java
@@ -0,0 +1,11 @@
+package hw_170712;
+
+public class UseStringStack {
+
+	public static void main(String[] args) {
+
+		StringStack stack = new StringStack();
+		
+	}
+
+}
