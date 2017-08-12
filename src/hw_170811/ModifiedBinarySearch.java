074545e97656d6def1b124e6caa9277a4bd1c621
sysloparich <sysloparich@gmail.com>
2017-08-12 10:26:58 +0300

    added an old version of bsearch

---
+++ src/hw_170811/ModifiedBinarySearch.java
@@ -0,0 +1,45 @@
+package hw_170811;
+
+import java.util.Random;
+
+public class ModifiedBinarySearch {
+	
+	public static int binarySearch(int[] a, int key){
+		
+		int lo = 0;
+		int hi = a.length - 1;
+		while(lo <= hi){
+			int mid = lo + (hi-lo)/2;
+			if(key < a[mid]) {
+				hi = mid - 1;
+			}
+			else {
+				if(key > a[mid]) {
+					lo = mid + 1;
+				}
+				else {
+					return mid;
+				}
+			}
+		}
+		
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		
+		int[] a = new int[20];
+		
+		for(int i = 0; i < a.length; ++i){
+			a[i] = r.nextInt(20);
+		}
+		
+		for(int i = 0; i < a.length; ++i){
+			System.out.print(a[i] + " ");
+		}
+		
+	}
+
+}
