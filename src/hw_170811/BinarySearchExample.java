a65ca308a0d25e1f07a6d4ee9b4b732c1b2f1b27
sysloparich <sysloparich@gmail.com>
2017-08-12 10:15:57 +0300

    added full time

--- src/hw_170811/BinarySearchExample.java
+++ src/hw_170811/BinarySearchExample.java
@@ -15,59 +15,78 @@ public class BinarySearchExample {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Addition and Sorting time in Millis");
+		System.out.println("All time in Nanos");
+		
+		long llist = 0;
+		long alist = 0;
+		long tset = 0;
 
 		LinkedList<A> linkedList = new LinkedList<>();	
 		Instant start = Instant.now();		
 		addElements(linkedList);		
 		Instant stop = Instant.now();
-		System.out.println("LinkedList add 1_000_000 elements : " + Duration.between(start, stop).toMillis());
+		llist += Duration.between(start, stop).toNanos();
+		System.out.println("LinkedList add 1_000_000 elements : " + Duration.between(start, stop).toNanos());
 		
 		ArrayList<A> arrayList = new ArrayList<>();		
 		start = Instant.now();	
 		addElements(arrayList);	
 		stop = Instant.now();
-		System.out.println("ArrayList add 1_000_000 elements : " + Duration.between(start, stop).toMillis());	
+		alist += Duration.between(start, stop).toNanos();
+		System.out.println("ArrayList add 1_000_000 elements : " + Duration.between(start, stop).toNanos());	
 		
 		start = Instant.now();
 		Collections.sort(linkedList);
 		stop = Instant.now();
-		System.out.println("LinkedList sort 1_000_000 elements : " + Duration.between(start, stop).toMillis());
+		llist += Duration.between(start, stop).toNanos();
+		System.out.println("LinkedList sort 1_000_000 elements : " + Duration.between(start, stop).toNanos());
 		
 		start = Instant.now();
 		Collections.sort(arrayList);
 		stop = Instant.now();
-		System.out.println("ArrayList sort 1_000_000 elements : " + Duration.between(start, stop).toMillis());		
+		alist += Duration.between(start, stop).toNanos();
+		System.out.println("ArrayList sort 1_000_000 elements : " + Duration.between(start, stop).toNanos());		
 		
 		
 		TreeSet<A> treeSet = new TreeSet<>();
 		start = Instant.now();		
 		addElements(treeSet);			
 		stop = Instant.now();
-		System.out.println("TreeSet add (and sort) 1_000_000 elements : " + Duration.between(start, stop).toMillis());
+		tset += Duration.between(start, stop).toNanos();
+		System.out.println("TreeSet add (and sort) 1_000_000 elements : " + Duration.between(start, stop).toNanos());
 				
 		A key = new A();
 		
-		System.out.println("Binary Search time in Nanos");
 		System.out.println("Searching element : " + key.x);
 		
 		start = Instant.now();
 		int index = Collections.binarySearch(linkedList, key);
 		stop = Instant.now();
+		llist += Duration.between(start, stop).toNanos();
 		System.out.println("Index of element: " + index + "; LinkedList search : " + 
 							Duration.between(start, stop).toNanos());
 		
 		start = Instant.now();
 		index = Collections.binarySearch(arrayList, key);
 		stop = Instant.now();
+		alist += Duration.between(start, stop).toNanos();
 		System.out.println("Index of element: " + index + "; ArrayList search : " + 
 							Duration.between(start, stop).toNanos());
 		
 		start = Instant.now();
 		boolean contains = treeSet.contains(key);
 		stop = Instant.now();
+		tset += Duration.between(start, stop).toNanos();
 		System.out.println("Contains? " + contains + " TreeSet search : " + 
-							Duration.between(start, stop).toNanos());						
+							Duration.between(start, stop).toNanos());
+		
+		System.out.println("=========================");
+		
+		System.out.println("Full Time:");
+		
+		System.out.println("LinkedList " + llist);
+		System.out.println(" ArrayList " + alist);
+		System.out.println("   TreeSet " + tset);
 		
 	}
 
